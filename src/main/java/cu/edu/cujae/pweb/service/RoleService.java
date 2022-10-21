c8c9ce6f2b7042c101c6127f5c236255948bbcc1
rachelpz <rachelpzro@gmail.com>
2022-10-21 18:04:25 -0400

    configCrudBrand1.7

---
+++ src/main/java/cu/edu/cujae/pweb/service/RoleService.java
@@ -0,0 +1,14 @@
+package cu.edu.cujae.pweb.service;
+
+
+
+import cu.edu.cujae.pweb.dto.RoleDto;
+
+import java.util.List;
+
+public interface RoleService {
+	List<RoleDto> getRoles();
+	List<RoleDto> getRolesByUser(Long userId);
+	List<RoleDto> getRolesByName(String name);
+	RoleDto getRolesById(Long roleId);
+}
---
+++ src/main/java/cu/edu/cujae/pweb/service/UserService.java
@@ -0,0 +1,15 @@
+package cu.edu.cujae.pweb.service;
+
+
+
+import cu.edu.cujae.pweb.dto.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+	List<UserDto> getUsers();
+	UserDto getUserById(String userId);
+	void createUser(UserDto user);
+	void updateUser(UserDto user);
+	void deleteUser(String id);
+}
