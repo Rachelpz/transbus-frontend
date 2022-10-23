5a8e4b521ba5a5f18761b0a8c95c5a2370b11cb9
kaitozx0 <iamstraxer44@gmail.com>
2022-10-23 11:41:44 -0400

    Finished Driver

--- src/main/java/cu/edu/cujae/pweb/bean/ManageDriverBean.java
+++ src/main/java/cu/edu/cujae/pweb/bean/ManageDriverBean.java
@@ -44,6 +44,7 @@ public class ManageDriverBean {
         drivers = drivers == null ? driverService.getDrivers() : drivers;
 
     }
+
     //Se ejecuta al dar clic en el button Nuevo
     public void openNew() {
 
@@ -55,5 +56,64 @@ public class ManageDriverBean {
 
     }
 
+    public void saveDriver() {
+        if (this.selectedDriver.getDriver_id() == null) {
+            this.selectedDriver.setDriver_id(UUID.randomUUID().toString().replaceAll("-", "").substring(0, 9));
+            this.selectedDriver.setNewRecord(true);
 
+
+            this.drivers.add(this.selectedDriver);
+            JsfUtils.addMessageFromBundle(null, FacesMessage.SEVERITY_INFO, "message_driver_added"); //Este code permite mostrar un mensaje exitoso (FacesMessage.SEVERITY_INFO) obteniendo el mensage desde el fichero de recursos, con la llave message_user_added
+        }
+        else {
+            JsfUtils.addMessageFromBundle(null, FacesMessage.SEVERITY_INFO, "message_driver_edited");
+        }
+
+        PrimeFaces.current().executeScript("PF('manageDriverDialog').hide()");//Este code permite cerrar el dialog cuyo id es manageUserDialog. Este identificador es el widgetVar
+        PrimeFaces.current().ajax().update("form:dt-drivers");// Este code es para refrescar el componente con id dt-users que se encuentra dentro del formulario con id form
+    }
+
+    public void deleteDriver() {
+        try {
+            this.drivers.remove(this.selectedDriver);
+            this.selectedDriver = null;
+            JsfUtils.addMessageFromBundle(null, FacesMessage.SEVERITY_INFO, "message_driver_removed");
+            PrimeFaces.current().ajax().update("form:dt-drivers"); // Este code es para refrescar el componente con id dt-users que se encuentra dentro del formulario con id form
+        } catch (Exception e) {
+            JsfUtils.addMessageFromBundle(null, FacesMessage.SEVERITY_ERROR, "message_error");
+        }
+
+    }
+
+    public DriverDto getDriverDto() {
+        return driverDto;
+    }
+
+    public void setDriverDto(DriverDto driverDto) {
+        this.driverDto = driverDto;
+    }
+
+    public DriverDto getSelectedDriver() {
+        return selectedDriver;
+    }
+
+    public void setSelectedDriver(DriverDto selectedDriver) {
+        this.selectedDriver = selectedDriver;
+    }
+
+    public List<DriverDto> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(List<DriverDto> drivers) {
+        this.drivers = drivers;
+    }
+
+    public DriverService getDriverService() {
+        return driverService;
+    }
+
+    public void setDriverService(DriverService driverService) {
+        this.driverService = driverService;
+    }
 }
