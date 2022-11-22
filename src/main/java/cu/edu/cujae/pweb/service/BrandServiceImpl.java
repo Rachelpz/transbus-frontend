26296ec33355fc336dd37a899bb5f406d4d8ed44
Rachel Lopez <rachelpzro@gmail.com>
2022-11-21 20:38:41 -0500

    configbackend.v.04

--- src/main/java/cu/edu/cujae/pweb/service/BrandServiceImpl.java
+++ src/main/java/cu/edu/cujae/pweb/service/BrandServiceImpl.java
@@ -1,7 +1,15 @@
 package cu.edu.cujae.pweb.service;
 
+import cu.edu.cujae.pweb.dto.UserDto;
+import cu.edu.cujae.pweb.utils.ApiRestMapper;
+import cu.edu.cujae.pweb.utils.RestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import cu.edu.cujae.pweb.dto.BrandDto;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -9,14 +17,25 @@ import java.util.UUID;
 @Service
 public class BrandServiceImpl implements BrandService{
 
+
+    @Autowired
+    private RestService restService;
+
     @Override
     public List<BrandDto> getBrands() {
 
-        List<BrandDto> brands = new ArrayList<>();
-        brands.add(new BrandDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Mercedes", 5, 45.0, "Gasolina", false));
-        brands.add(new BrandDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)) , "Audi", 8, 35.0, "Gasolina", false));
+        List<BrandDto> brands = new ArrayList<BrandDto>();
 
+        try {
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            ApiRestMapper<BrandDto> apiRestMapper = new ApiRestMapper<>();
+            String response = (String)restService.GET("/api/v1/brands", params, String.class).getBody();
+            brands = apiRestMapper.mapList(response, BrandDto.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return brands;
+
     }
 
     @Override
