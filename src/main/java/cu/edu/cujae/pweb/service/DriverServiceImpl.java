bac8e4d71a98619d81aec95c9929cf71623bb782
EdBernal00 <edbernal00@gmail.com>
2022-11-26 19:22:02 -0500

    Added more Drivers Instances for testing Carousel in Dashboard

--- src/main/java/cu/edu/cujae/pweb/service/DriverServiceImpl.java
+++ src/main/java/cu/edu/cujae/pweb/service/DriverServiceImpl.java
@@ -17,6 +17,13 @@ public class DriverServiceImpl implements DriverService{
         List<DriverDto> drivers = new ArrayList<>();
         drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 1", "00021060502", "KSA", "55654413", "Distrito 1", "Marca 1", false ));
         drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 2", "98021235102", "KSA", "55673513", "Distrito 2", "Marca 2", false ));
+        drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 3", "00012487635", "KSA", "55673513", "Distrito 2", "Marca 3", false ));
+        drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 4", "03051145698", "KSA", "55673513", "Distrito 2", "Marca 4", false ));
+        drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 5", "00071966627", "KSA", "55673513", "Distrito 2", "Marca 5", false ));
+        drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 6", "95072021365", "KSA", "55673513", "Distrito 2", "Marca 6", false ));
+        drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 7", "96021835103", "KSA", "55673513", "Distrito 2", "Marca 7", false ));
+        drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 8", "91021935102", "KSA", "55673513", "Distrito 2", "Marca 8", false ));
+        drivers.add(new DriverDto(Integer.valueOf(UUID.randomUUID().toString().replaceAll("-|[a-zA-Z]", "").substring(0, 6)), "Driver 9", "90022735129", "KSA", "55673513", "Distrito 2", "Marca 9", false ));
 
         return drivers;
     }
